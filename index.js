16579923358a0f31a5c171ac94fe679082ec5953
nmccready <nemtcan@gmail.com>
2018-04-27 16:16:12 -0400

    reduced to single test suite

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*!
- * Middleware Emitter.
- *
- * Middleware Emitter Class.
- * @created 27/03/2017 NZDT
- */
-
-
-/**
- * Module exports.
- */
-
 module.exports = require('./lib/middleware-emitter');
 
-module.exports.wrap = require('./lib/middleware-wrap-emitter');
+module.exports.Wrap = require('./lib/middleware-wrap-emitter');
