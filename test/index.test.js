243411ae2dfe0494b1e54233472f9808c8ae263e
nmccready <nemtcan@gmail.com>
2018-04-30 18:10:30 -0400

    next paradigm switched to koa-compose to be like koa and support promises. Eliminates callback hell

--- test/index.test.js
+++ test/index.test.js
@@ -1,106 +1,159 @@
 const test = require('ava');
 const EventEmitter = require('events');
 const MiddleWrapEmitter = require('../');
+const Bluebird = require('bluebird');
+
+global.Promise = Bluebird;
 
 [
   new MiddleWrapEmitter({}),
   new MiddleWrapEmitter({}, new EventEmitter())
 ].forEach((emitter) => {
-  test('fist', (t) => {
+  test.cb('all middle ware is called and handled', (t) => {
     let total = 0;
     const expectedRes1 = { ctx: { one: true, two: true } };
     const expectedReq1 = {
-      ctx: { hello: 'world', three: true },
-      event: { name: 'test' }
+      ctx: { hello: 'world', three: true }
     };
 
     emitter
       .on(
         ['test', 'hello'],
-        ({ req, res }, next) => {
+        async ({ req, res }, next) => {
+          try {
+            await next();
+          } catch (err) {
+            t.deepEqual(err.message, 'Custom Error.', 'Error is Caught') ||
+              total++;
+            t.is(total, 4, 'total');
+            t.end();
+          }
+        },
+        async ({ req, res }, next) => {
           res.ctx.one = true;
-          next();
+          await next();
         },
-
-        ({ req, res }, next) => {
+        async ({ req, res }, next) => {
           res.ctx.two = true;
-          next();
+          await next();
         },
-
-        {
-          three: true
+        async ({ req, res }, next) => {
+          req.ctx.three = true;
+          await next();
         },
-
-        ({ req, res }, next) => {
-          next(new Error('Custom Error.'));
+        async ({ req, res }, next) => {
+          await next(); // allows next callback (below) to run
+          throw new Error('Custom Error.');
         },
-
-        ({ req, res }, next, err) => {
-          t.deepEqual(err.message, 'Custom Error.') || total++;
-          next();
-        },
-
-        ({ req, res }) => {
-          t.deepEqual(req, expectedReq1) || total++;
-          t.deepEqual(res, expectedRes1) || total++;
+        async ({ req, res, event: { name } }, next) => {
+          t.deepEqual(req, expectedReq1, 'request') || total++;
+          t.deepEqual(res, expectedRes1, 'response') || total++;
+          t.deepEqual(name, 'test', 'event name') || total++;
+          await next();
         }
       )
       .emit(['test'], { hello: 'world' });
-
-    t.is(total, 3, 'total');
-    t.pass();
   });
 
-  test('second', (t) => {
+  test.cb('emits all events across all handlers', (t) => {
     let total = 0;
-
-    emitter.emit('hello');
 
     const events = ['t1', 't2', 't3', 't4', 't5'];
 
     emitter
-      .on(
-        events,
+      .on(events, async ({ req, event: { name } }) => {
+        t.truthy(events.includes(name));
+        t.deepEqual(req.ctx.some, 'data') || total++;
 
-        ({ req }) => {
-          t.truthy(events.includes(req.event.name)) || total++;
-          t.deepEqual(req.ctx.some, 'data') || total++;
+        if (events.length === total) {
+          t.end();
         }
-      )
+      })
       .emit(events, { some: 'data' });
+  });
+
+  test.cb('specific errors can be handled (async)', (t) => {
+    let total = 0;
+
+    class CustomError1 extends Error {}
+    class CustomError2 extends Error {}
 
     emitter
       .on(
-        'errors',
-        ({ req, res }, next) => {
-          next(new Error('Custom error 1'));
+        ['errors2', 'errors1'],
+        async ({ req, res }, next) => {
+          try {
+            await next();
+          } catch (err) {
+            if (err instanceof CustomError2) {
+              t.deepEqual(err.message, 'Custom error 2', 'catch error2') ||
+                total++;
+            } else if (err instanceof CustomError1) {
+              t.deepEqual(err.message, 'Custom error 1', 'catch error1') ||
+                total++;
+              t.deepEqual(total, 2, 'total errors');
+              t.end();
+            } else {
+              t.fail(err);
+              t.pass();
+            }
+          }
         },
-        ({ req, res }, next, err) => {
-          t.deepEqual(err.message, 'Custom error 1') || total++;
-          next();
+        async ({ req, res, event: { name } }, next) => {
+          await next();
+          if (name === 'errors1') throw new CustomError1('Custom error 1');
         },
-        ({ req, res }, next) => {
-          next(new Error('Custom error 2'));
+        async ({ req, res, event: { name } }, next) => {
+          await next();
+          if (name === 'errors2') throw new CustomError2('Custom error 2');
         },
-        ({ req, res }, next, err) => {
-          t.deepEqual(err.message, 'Custom error 2') || total++;
+        async (_, next) => {
+          await next();
         }
       )
-      .emit('errors');
+      .emit('errors2')
+      .emit('errors1');
+  });
+
+  test.cb('specific errors can be handled (Bluebird)', (t) => {
+    let total = 0;
+
+    class CustomError1 extends Error {}
+    class CustomError2 extends Error {}
 
     emitter
       .on(
-        'more-errors',
-        ({ req, res }, next, err) => {
-          t.deepEqual(err.message, 'Custom error') || total++;
-        },
+        ['errors2', 'errors1'],
         ({ req, res }, next) => {
-          next(new Error('Custom error'));
+          return next() // BLUEBIRD SUGAR
+            .catch(CustomError2, (err) => {
+              t.deepEqual(err.message, 'Custom error 2', 'catch error2') ||
+                total++;
+            })
+            .catch(CustomError1, (err) => {
+              t.deepEqual(err.message, 'Custom error 1', 'catch error1') ||
+                total++;
+              t.deepEqual(total, 2, 'total errors');
+              t.end();
+            })
+            .catch(CustomError2, (err) => {
+              t.fail(err);
+              t.end();
+            });
+        },
+        async ({ req, res, event: { name } }, next) => {
+          await next();
+          if (name === 'errors1') throw new CustomError1('Custom error 1');
+        },
+        async ({ req, res, event: { name } }, next) => {
+          await next();
+          if (name === 'errors2') throw new CustomError2('Custom error 2');
+        },
+        async (_, next) => {
+          await next();
         }
       )
-      .emit('more-errors');
-
-    t.true(total > 0, 'total');
-    t.pass();
+      .emit('errors2')
+      .emit('errors1');
   });
 });
